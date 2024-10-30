8cb24c29f6c47997e20c80a1f7b538da504d946a
Tristan Saidi <tls2160@columbia.edu>
2024-10-30 15:36:29 -0400

    isomap objective compatible with > 1 cc

--- src/isorc.py
+++ src/isorc.py
@@ -58,66 +58,45 @@ class ISORC(object):
     def _ann_summary(self, G):
         """
         Get information regarding orcmanl annotations.
-        Parameters
-        ----------
-        G : networkx.Graph
-            The annotated graph.
-        Returns
-        -------
-        sc_ind : np.ndarray
-            The shortcut indicator matrix.
-        D_G_prime : np.ndarray
-            The G_prime distance matrix.
         """
-        # create two |V| x |V| matrices
-        n = len(G.nodes)
-        sc_ind = np.zeros((n, n))
-        D_G_prime = np.zeros((n, n))
+        shortcut_indices = []
+        non_shortcut_indices = []
 
         # iterate through edges of G
         for u, v in G.edges:
             # get the isorc annotation of the edge: shortcut
             sc = G[u][v]['shortcut']
-            sc_ind[u, v] = sc
-            sc_ind[v, u] = sc
-            # get the isorc annotation of the edge: distance
-            d = G[u][v]['G_prime_dist']
-            D_G_prime[u, v] = d
-            D_G_prime[v, u] = d    
+            if sc:
+                shortcut_indices.append([u, v])
+            else:
+                non_shortcut_indices.append([u, v])
 
-        self.D_G_prime = D_G_prime
-        self.sc_ind = sc_ind # shortcut indicator mask matrix
-        self.non_sc_mask = ~sc_ind.astype(bool) # non-shortcut indicator mask matrix
-        # create torch versions
-        self.D_G_prime_torch = torch.tensor(D_G_prime).to(self.device)
-        self.sc_ind_torch = torch.tensor(sc_ind).to(self.device)
-        self.non_sc_mask_torch = torch.tensor(self.non_sc_mask).to(self.device)
-        # create a connected component mask by computing pairwise distances with adjacency matrix
+        # create shortcut indices matrix
+        shortcut_indices = torch.tensor(shortcut_indices).to(self.device)
+        non_shortcut_indices = torch.tensor(non_shortcut_indices).to(self.device)
+
         self.geo_dist = scipy.sparse.csgraph.shortest_path(self.A, directed=False)
-        # set infinities to zero, all other values to 1
-        self.connected_comp_mask = torch.tensor(self.geo_dist < np.inf).to(self.device)
         self.geo_dist = torch.tensor(self.geo_dist).to(self.device)
-        # replace infinities with 0
-        self.masked_geo_dist = torch.where(self.geo_dist == np.inf, torch.tensor(0.0).to(self.device), self.geo_dist)
-        # get max value of nonshortcut edges
-        self.max_val = torch.max(self.geo_dist[self.geo_dist < np.inf])
 
-    def _eq_matrix(self, dist_scale=1.0):
-        """
-        Compute the spring equilibrium matrix for the isorc embedding.
-        Parameters
-        ----------
-        dist_scale : float, optional
-            The distance scaling factor.
-        """
-        # create the equilibrium matrix as element-wise maximum of A and sc_ind * D_G_prime
-        E = np.maximum(self.A.cpu().numpy(), self.sc_ind * self.D_G_prime * dist_scale)
-        self.E = torch.tensor(E).to(self.device)
+        # max non-shortcut distance
+        self.max_non_shortcut_dist = torch.max(self.geo_dist[self.geo_dist != np.inf]).detach()
+        print(f"Max non-shortcut distance: {self.max_non_shortcut_dist}")
+
+        # target distances are geo distances, and max non-shortcut distance for shortcut edges where geo distance is infinite
+        self.target_dist = self.geo_dist.clone().requires_grad_(False)
+        for u, v in shortcut_indices:
+            if self.target_dist[u, v] == np.inf:
+                self.target_dist[u, v] = self.max_non_shortcut_dist
+        # create a mask of noninf values
+        self.noninf_mask = self.target_dist != np.inf
+        self.noninf_mask = self.noninf_mask.to(self.device).requires_grad_(False)
+        # fill in inf values with 0 now for loss computation
+        self.target_dist[self.target_dist == np.inf] = 0
 
     def fit(
             self,
             n_iter=1000,
-            beta=0.0,
+            beta=0,
             lr=0.1,
             frames=10
     ):
@@ -133,7 +112,7 @@ class ISORC(object):
         # optimize X so that pairwise distances are close to the equilibrium distances
         self.X_opt = torch.tensor(
             self.orcmanl.X
-        ).to(self.device).requires_grad_(True)
+        ).float().to(self.device).requires_grad_(True)
         # optimizer and lr scheduler
         optimizer = torch.optim.Adam([self.X_opt], lr=lr)
         # animation
@@ -144,24 +123,18 @@ class ISORC(object):
                 optimizer.zero_grad()
                 pdist = torch.cdist(self.X_opt, self.X_opt, p=2)
                 fro_norm = torch.norm(pdist)
-                masked_pdist = pdist * self.connected_comp_mask
-                # squared displacement for same cc pairs
-                D_sq_same_cc = (masked_pdist - self.masked_geo_dist)**2
-                # squared displacement for shortcut edges
-                print(self.max_val)
-                D_sq_sc = (masked_pdist - self.max_val)**2 * self.sc_ind_torch
-                # clamp D_sq_sc so max value is max value of nonshortcut edges
-                D_sq_sc = torch.clamp(D_sq_sc, max=self.max_val)
-                # compute loss
-                loss_sc = torch.sum(D_sq_sc)
-                loss_same_cc = torch.sum(D_sq_same_cc)
+                squared_diff = (pdist - self.target_dist) ** 2
+                # only consider target pairs with noninf geo distances
+                squared_diff = torch.masked_select(squared_diff, self.noninf_mask)
+                loss_geo = torch.sum(squared_diff)
                 loss_spread = - beta * fro_norm
-                loss = loss_sc + loss_spread
+                loss = loss_geo + loss_spread
                 loss.backward()
+
                 optimizer.step()
                 if i % steps_per_frame == 0:
                     frames.append(self.X_opt.clone().detach().cpu().numpy())
-                pbar.set_postfix({'loss': loss.item(), 'loss_sc': loss_sc.item(), 'loss_same_cc': loss_same_cc.item(), 'loss_spread': loss_spread.item()})
+                pbar.set_postfix({'loss': loss.item(), 'loss_geo': loss_geo.item(), 'loss_spread': loss_spread.item()})
                 pbar.update(1)
         return self.X_opt.detach().cpu().numpy(), frames
 
